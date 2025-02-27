a7f24755d76b2dc5d8baa5363ad634cff554248f
Paulo <pescanellasotero@gmail.com>
2025-02-27 10:45:03 +0100

    Creo atributos en la clase Tabla con los cuales puedo crear un metodo que me permite imprimir el tablero de Juego

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,2 +1,16 @@
 public class Tablero {
+    private char[][] tablero;
+    private final int filas = 9;
+    private final int columnas = 9;
+
+    public void imprimirTablero(){
+        Tablero tablero = new Tablero();
+
+        for (int i = 0; i < filas; i++){
+            for (int j = 0; j < columnas; j++){
+                System.out.print("[" + ' ' + "]");
+            }
+            System.out.println();
+        }
+    }
 }
