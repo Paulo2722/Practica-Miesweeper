6e2d89aa0f1e88f4a3f2b21ddbbe8271d1874f33
Paulo <pescanellasotero@gmail.com>
2025-02-27 10:37:35 +0100

    Comienzo con la creación e inicialización de los atributos de las clases Jugador y Ficha

--- src/Ficha.java
+++ src/Ficha.java
@@ -1,2 +1,15 @@
 public class Ficha {
+    private char ficha;
+
+    public Ficha(char ficha){
+        this.ficha = ficha;
+    }
+
+    public char getFicha(){
+        return ficha;
+    }
+
+    public void setFicha(char ficha){
+        this.ficha = ficha;
+    }
 }
--- src/Jugador.java
+++ src/Jugador.java
@@ -1,2 +1,16 @@
 public class Jugador {
+    private String nombre;
+
+    public Jugador(String nombre, char ficha){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
 }
