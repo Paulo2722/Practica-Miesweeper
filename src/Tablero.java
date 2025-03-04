9ead24582b1408a16d5096f06ff73accda68653e
Paulo <pescanellasotero@gmail.com>
2025-03-04 13:17:00 +0100

    Vuelvo a crear el método imprimirTablero con el cual podré imprimir el tablero pero con los valores actualizados de cada casilla. También, he creado un método ponerMinas con el cual puedo colocar en casillas aleatorias del tablero, las minas del juego, las cuales me servirán para establecer diferentes funcionalidades en un futuro

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Tablero {
     private char[][] tablero;
     private final int filas = 9;
     private final int columnas = 9;
-    private final int numeroMinas = 10;
     private char blanco = ' ';
+    private char Mina = 'O';
 
     public Tablero() {
 
@@ -15,4 +18,31 @@ public class Tablero {
             }
         }
     }
+
+    public void imprimirTablero(){
+
+    for (int i = 0; i < filas; i++){
+        for (int j = 0; j < columnas; j++){
+            System.out.print("[" + tablero[i][j] + "]");
+        }
+        System.out.println();
+    }
+    }
+
+    public void ponerMinas(){
+
+        int numeroMinas = 10;
+
+        do{
+            Random random = new Random();
+
+            int filaAleatoria = random.nextInt(10);
+            int columnaAleatoria = random.nextInt(10);
+
+            tablero[filaAleatoria][columnaAleatoria] = Mina;
+            numeroMinas--;
+
+        }while (numeroMinas != 0);
+    }
+
 }
