4f1b29954808e21d3f52cd45bd0aa87b9894b18b
Paulo <pescanellasotero@gmail.com>
2025-03-13 10:28:49 +0100

    Codifico la clase Juego para poder iniciar una partida y jugar al buscaminas hasta que el jugador gane o pierda. Para esto, he tenido que eliminar la clase 'seleccionarOpcion' y mover parte de su contenido a la clase 'Juego', además de que he tenido que añadir los métodos poner y quitar Bandera para que funcionasen

--- src/Juego.java
+++ src/Juego.java
@@ -5,6 +5,73 @@ public class Juego {
     public void iniciarJuego(){
         Scanner sc = new Scanner(System.in);
 
-        Ficha ficha = new Ficha('P', '?');
+        Tablero tablero = new Tablero();
+        tablero.ponerMinas();
+        boolean partida = true;
+
+        while (partida) {
+
+            tablero.imprimirTablero();
+
+            System.out.println("Seleccione su acción (1-3): ");
+            System.out.println();
+            System.out.println("1.Abrir casilla");
+            System.out.println("2.Poner bandera");
+            System.out.println("3.Quitar bandera");
+            System.out.println("4.Finalizar partida");
+
+            int opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+
+                    int posicionFila = sc.nextInt();
+                    int posicionColumna = sc.nextInt();
+
+                    System.out.println("Seleccione una fila (0-8): " + posicionFila);
+                    System.out.println("Seleccione una columna (0-8): " + posicionColumna);
+
+                    tablero.revelarCasillas(posicionFila, posicionColumna);
+                    tablero.comprobarVictoria();
+
+                    if (tablero.comprobarVictoria()) {
+                        partida = false;
+                    }
+
+                    break;
+
+                case 2:
+
+                    posicionFila = sc.nextInt();
+                    posicionColumna = sc.nextInt();
+
+                    System.out.println("Seleccione una fila (0-8): " + posicionFila);
+                    System.out.println("Seleccione una columna (0-8): " + posicionColumna);
+
+                    tablero.ponerBandera(posicionFila, posicionColumna);
+
+                    break;
+
+                case 3:
+
+                    posicionFila = sc.nextInt();
+                    posicionColumna = sc.nextInt();
+
+                    System.out.println("Seleccione la fila de la bandera (0-8): " + posicionFila);
+                    System.out.println("Seleccione la columna de la bandera (0-8): " + posicionColumna);
+
+                    tablero.quitarBandera(posicionFila, posicionColumna);
+
+                    break;
+
+                case 4:
+
+                    partida = false;
+
+                default:
+                    System.out.println("La opción seleccionada no es válida");
+            }
+        }
+        System.out.println("Partida finalizada");
     }
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -52,44 +52,14 @@ public class Tablero {
         } while (minasPorPoner != 0);
     }
 
-    public void seleccionarOpcion(int opcion, int posicionFila, int posicionColumna) {
+    public void ponerBandera(int posicionFila, int posicionColumna){
+        tablero[posicionFila][posicionColumna].ponerBandera();
+    }
 
-        System.out.println("Seleccione su acción (1-3): ");
-        System.out.println();
-        System.out.println("1.Abrir casilla");
-        System.out.println("2.Poner bandera");
-        System.out.println("3.Quitar bandera");
+    public void quitarBandera(int posicionFila, int posicionColumna){
 
-        switch (opcion) {
-            case 1:
-                System.out.println("Seleccione una fila (0-8): " + posicionFila);
-                System.out.println("Seleccione una columna (0-8): " + posicionColumna);
-
-                tablero[posicionFila][posicionColumna].abrir();
-                revelarCasillas(posicionFila, posicionColumna);
-                comprobarVictoria();
-
-                break;
-
-            case 2:
-                System.out.println("Seleccione una fila (0-8): " + posicionFila);
-                System.out.println("Seleccione una columna (0-8): " + posicionColumna);
-
-                tablero[posicionFila][posicionColumna].ponerBandera();
-
-                break;
-
-            case 3:
-                System.out.println("Seleccione la fila de la bandera (0-8): " + posicionFila);
-                System.out.println("Seleccione la columna de la bandera (0-8): " + posicionColumna);
-
-                if (tablero[posicionFila][posicionColumna].tieneBandera()){
-                    tablero[posicionFila][posicionColumna].quitarBandera();
-                }
-
-            default:
-                System.out.println("La opción seleccionada no es válida");
-        }
+        if (tablero[posicionFila][posicionColumna].tieneBandera())
+            tablero[posicionFila][posicionColumna].quitarBandera();
     }
 
     public int contarMinasAdyacentes(int posicionFila, int posicionColumna) {
